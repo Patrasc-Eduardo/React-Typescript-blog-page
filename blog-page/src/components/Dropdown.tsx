3dd0001ae8d0ad6958841c60e611f8f2490a09bc
Patrasc-Eduardo <patrasceduard@gmail.com>
2022-05-25 16:52:52 +0300

    improved dropdown

--- blog-page/src/components/Dropdown.tsx
+++ blog-page/src/components/Dropdown.tsx
@@ -10,6 +10,7 @@ function Dropdown (props: DropdownProps) {
     const {articles} = props;
     return (
         <select name="authors" id="authors">
+         <option>Select an author</option>
          {
           articles.map(article => {
             return (
